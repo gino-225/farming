7a3d72485c4bacebf67745b9fbb3d51496534575
gino-225 <george_bm@aol.com>
2018-06-13 17:48:19 +0100

    created fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180613162431) do
+ActiveRecord::Schema.define(version: 20180613164532) do
 
   create_table "crops", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "fields", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "title"
+    t.text "size"
+    t.integer "number"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
